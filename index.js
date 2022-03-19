cf2032869a4e0b3a7368fc59f7a6af11d511f7e8
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-03-18 21:52:23 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    if (blocks === 43) { return 1}
+    if (blocks === 34) {return 8
+        
+    } else {return 8}
+        
+     
+
+        
+    
+}
+      function distanceFromHqInFeet(blocks){
+        if (blocks === 43) { return 264}
+        if (blocks === 34) {return 2112
+            
+        } else {return 2112}
+      }
+
+      function distanceTravelledInFeet(x,y){
+          if ((x-y) > 0) {return (x-y)*264}
+              
+           
+          else return (y-x)*264
+              
+          
+      }
+    function calculatesFarePrice(x,y){
+        if( Math.abs(y - x)*264 < 400) {return 0}
+        if( Math.abs(y - x)*264  < 2000) {return 2.56}
+        else if ( Math.abs(y - x)*264 < 2500) {return 25}
+        else  {return 'cannot travel that far'}
+            
+        
+    }
